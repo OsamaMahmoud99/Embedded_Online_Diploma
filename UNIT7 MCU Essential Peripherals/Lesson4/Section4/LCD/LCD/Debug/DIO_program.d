62d80d7f4d7c82ba546c81a85d2bb5553c10c938
Osama mahmoud <70059286+OsamaMahmoud99@users.noreply.github.com>
2021-07-20 18:19:01 +0200

    Add files via upload

---
+++ UNIT7 MCU Essential Peripherals/Lesson4/Section4/LCD/LCD/Debug/DIO_program.d
@@ -0,0 +1,12 @@
+DIO_program.d DIO_program.o: .././DIO_program.c .././DIO_interface.h \
+ .././STD_TYPES.h .././BIT_MATH.h .././Mem_Map32.h .././DIO_config.h
+
+.././DIO_interface.h:
+
+.././STD_TYPES.h:
+
+.././BIT_MATH.h:
+
+.././Mem_Map32.h:
+
+.././DIO_config.h:
---
+++ UNIT7 MCU Essential Peripherals/Lesson4/Section4/LCD/LCD/Debug/main.d
@@ -0,0 +1,33 @@
+main.d main.o: .././main.c .././Mem_Map32.h .././STD_TYPES.h \
+ .././BIT_MATH.h .././DIO_interface.h .././DIO_config.h \
+ .././LCD_interface.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+
+.././Mem_Map32.h:
+
+.././STD_TYPES.h:
+
+.././BIT_MATH.h:
+
+.././DIO_interface.h:
+
+.././DIO_config.h:
+
+.././LCD_interface.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
